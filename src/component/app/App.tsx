7b581173370bc140fb79b3e32d760516d27ec142
Pablo da Cunha <pablodacun@gmail.com>
2022-06-01 20:20:31 +0200

    Pintar

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { crearPersonajes } from '../../servicios/personajes';
+import { Lista } from '../lista/index';
+
 import './App.css';
 
 function App() {
+    const personajes = crearPersonajes();
+    console.log(personajes);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Lista personajes={personajes}/>
+
     </div>
   );
 }
--- src/servicios/personajes.ts
+++ src/servicios/personajes.ts
@@ -1,8 +1,8 @@
-import { Asesor } from '../models/asesor.js';
-import { Escudero } from '../models/escudero.js';
-import { Luchador } from '../models/luchador.js';
-import { Rey } from '../models/rey.js';
-import { Personaje } from '../models/personaje.js';
+import { Asesor } from '../models/asesor';
+import { Escudero } from '../models/escudero';
+import { Luchador } from '../models/luchador';
+import { Rey } from '../models/rey';
+import { Personaje } from '../models/personaje';
 
 export function crearPersonajes() {
     const data: Array<Personaje> = [
