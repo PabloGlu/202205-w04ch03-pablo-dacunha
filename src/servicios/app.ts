f3caae426dad8da027eb3715af990aed8ced2f3f
Pablo da Cunha <pablodacun@gmail.com>
2022-06-01 19:03:26 +0200

    Starting

---
+++ src/servicios/app.ts
@@ -0,0 +1,13 @@
+import { Card } from '../components/card.js';
+import { crearPersonajes } from './personajes.js';
+
+function app() {
+    const personajes = crearPersonajes();
+    console.log(personajes);
+    let selector = '.characters-list';
+    personajes.forEach((item) => new Card(item, selector));
+}
+
+(() => {
+    document.addEventListener('DOMContentLoaded', app);
+})();
